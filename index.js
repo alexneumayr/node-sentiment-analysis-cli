0d3457965f14f9f386c45a4535e1f3a0f52b7800
alexneumayr <alex.nmr@outlook.com>
2025-01-16 20:42:33 +0100

    Add comments

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 import fs from 'node:fs';
 import OpenAI from 'openai';
 
+// Create new OpenAI object to have access to the API
 const openai = new OpenAI({
   apiKey:
     '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',
 });
 
-const userInput = process.argv.slice(2).join(' ');
+const userInput = process.argv.slice(2).join(' '); // Makes it possible to use multiple words as an argument
+const regex = new RegExp('[.]txt$'); // Creates RegEx object to check for a string ending with ".txt"
+let stringToAnalyze;
 
+// Function to open a file and return it's content as a string
 function outputFileContent(fileName) {
   const fileInBuffer = fs.readFileSync(fileName);
   const fileAsText = fileInBuffer.toString();
   return fileAsText;
 }
 
-const regex = new RegExp('[.]txt$');
-let stringToAnalyze;
-
+// Check if the argument ends with ".txt"
 if (regex.test(userInput)) {
   try {
-    stringToAnalyze = outputFileContent(userInput);
+    stringToAnalyze = outputFileContent(userInput); // If it ends with ".txt" save file content to stringToAnalyze
   } catch (error) {
+    // Display error message if there was a problem processing the file
     console.log(
       'The following error occurred while processing the text file:\n',
       error,
     );
   }
 } else {
+  // If the argument doesn't end with ".txt" just assign it to stringToAnalyze
   stringToAnalyze = userInput;
 }
 
+// Create the gpt request and save it to completion
 const completion = openai.chat.completions.create({
-  model: 'gpt-4o-mini',
-  store: true,
-  messages: [{ role: 'user', content: stringToAnalyze }],
+  model: 'gpt-4o-mini', // use gpt-4o-mini
+  messages: [{ role: 'user', content: stringToAnalyze }], // use stringToAnalyze as the content
   response_format: {
+    // specify response scheme
     type: 'json_schema',
     json_schema: {
       name: 'sentiment_analysis',
@@ -44,7 +49,7 @@ const completion = openai.chat.completions.create({
           confidenceRating: {
             type: 'number',
             description:
-              'The percentage of how certain the AI is that it correctly detected the Sentiments.',
+              'The percentage of how certain the AI is that it correctly detected the sentiments.',
           },
           sentiments: {
             type: 'array',
@@ -98,17 +103,20 @@ const completion = openai.chat.completions.create({
   },
 });
 
+// Execute gpt request
 completion.then((result) => {
-  const responseContentJSON = JSON.parse(result.choices[0].message.content);
-  const confidenceRating = responseContentJSON.confidenceRating;
+  const responseContentJSON = JSON.parse(result.choices[0].message.content); // Save parsed JSON object
   const sentimentsArray = responseContentJSON.sentiments;
   const emotionsArray = responseContentJSON.emotions;
+  // Output confidence rating
   console.log(
     `Your text has the following sentiment (with ${responseContentJSON.confidenceRating}% certainty):\n`,
   );
+  // Loop through the sentiments array and output name and percentage
   sentimentsArray.forEach((sentiment) =>
     console.log(`- ${sentiment.percentage}% ${sentiment.name}`),
   );
+  // Loop through the emotions array and output name and percentage
   console.log('\nThe emotions found in the text are:\n');
   emotionsArray.forEach((emotion) =>
     console.log(`- ${emotion.name} (${emotion.percentage}%)`),
