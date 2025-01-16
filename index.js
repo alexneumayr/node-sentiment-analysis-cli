eb7f7bfcc2b6812f1c40d35c57e6e4fe3864ff79
alexneumayr <alex.nmr@outlook.com>
2025-01-16 21:34:26 +0100

    Add new comment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import fs from 'node:fs';
 import OpenAI from 'openai';
 
 // Create new OpenAI object to have access to the API
+// Important: The API key is now deactivated for security reasons after publishing on GitHub
 const openai = new OpenAI({
   apiKey:
     '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',
